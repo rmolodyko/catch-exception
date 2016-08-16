43b2aebf7b1fe7929832e31b20584b293f339e5a
Molodyko Ruslan <molodyko@samsonos.com>
2016-08-16 17:23:29 +0300

    Fixed catch exception test

--- tests/CatchExceptionListenerTest.php
+++ tests/CatchExceptionListenerTest.php
@@ -3,11 +3,24 @@
 namespace Satori\CatchException\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Satori\CatchException\Lib\CatchExceptionListener;
+use Satori\CatchException\Lib\Exception\AbstractException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 
 class CatchExceptionListenerTest extends TestCase
 {
-    public function testWrongInstantiating()
+    public function testOnKernelException()
     {
-        static::assertTrue(true);
+        $listener = new CatchExceptionListener();
+        $event = static::createMock(GetResponseForExceptionEvent::class);
+        $response = static::createMock(JsonResponse::class);
+        $exception = static::createMock(AbstractException::class);
+        $exception->expects(static::once())->method('getResponse')->willReturn($response);
+
+        $event->expects(static::once())->method('getException')->willReturn($exception);
+        $event->expects(static::once())->method('setResponse');
+
+        $listener->onKernelException($event);
     }
 }
